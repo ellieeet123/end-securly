d5416655c4cc0d545fde04b6ff2e3025f7837929
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2021-12-21 08:12:09 -0600

    Create rand_string.js

---
+++ rand_string.js
@@ -0,0 +1,13 @@
+/* [BOOKMARKLET] generates 16 character random string (ignore this pls) */
+
+javascript:
+function getRandStr(){
+  var values = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+  var output = '';
+  for (var i = 0; i > values.length;i++) {
+    output += values[Math.floor(Math.random()*values.length)];
+  }
+  return output;
+}
+
+alert(getRandStr());
