8bc0e7c4d96f69b53b82cbae233c0a2ac17f8de0
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2022-01-07 08:06:59 -0600

    testing some stuff

--- main_dev.js
+++ main_dev.js
@@ -8,6 +8,9 @@
 \*/ 
 
 javascript:
+function element(id) {
+    return document.getElementById(id)
+}
 function unblock() {
     var w = window.innerWidth;
     var h = window.innerHeight;
@@ -15,23 +18,28 @@ function unblock() {
     var html = '<!doctype html><html><body><iframe id="unblock_iframe" height="6" width="6" frameborder="0" style="position:fixed;right:0px;bottom:0px;"></iframe></body></html>'
     document.write('');
     document.write(html);
-    var f = document.getElementById('unblock_iframe');
+    var f = element('unblock_iframe');
     f.src = p;
     f.width = w;
     f.height = h;
     window.onresize = function() {
-        document.getElementById('unblock_iframe').width = window.innerWidth;
-        document.getElementById('unblock_iframe').height = window.innerHeight;        
+        element('unblock_iframe').width  = window.innerWidth;
+        element('unblock_iframe').height = window.innerHeight;        
     }
 }
 function openNewTab(url) {
-    var link = document.createElement('a');
-    link.href = url;
-    link.style.position = 'fixed';
-    link.style.top = '1000000px';
-    link.id = 'unblock_link';
-    link.target = '_blank';
-    link.click();
+    if (element('unblock_link')) {
+        element('unblock_link').click();
+    }
+    else {
+        var link = document.createElement('a');
+        link.href = url;
+        link.style.position = 'fixed';
+        link.style.top = '1000000px';
+        link.id = 'unblock_link';
+        link.target = '_blank';
+        link.click();
+    }
 }
 if (window.location.href == 'about:blank') {
     unblock();
