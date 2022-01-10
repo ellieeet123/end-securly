d7f3bdd09284d2acd8fee2e3b2339ec8f6968297
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2022-01-10 13:14:35 -0600

    more stuffs

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,16 @@
 javascript:
 (function() {
     function loadScript() {
-        document.addEventListener("securitypolicyviolation", function() {
-            alert('[ERROR] Security Violation')
-            // add more here eventually
-        })
+        document.addEventListener("securitypolicyviolation", (e) => {
+            if (confirm('[ERROR]: ' + window.location.hostname + 
+            ' has blocked inserting the necesary script for the unblocker. Would you like to open a blank tab to run it instead? (you will have to double click the bookmarklet again.) - Full Error Report: '
+            + e)) {
+                var a = document.createElement('a');
+                a.href = 'about:blank';
+                a.target = '_blank';
+                a.click()
+            }
+        });
         var script = document.createElement('script');
         script.src = "https://ellieeet123.github.io/end-securly/unblocker.js";
         script.id = 'unblockerscript';
