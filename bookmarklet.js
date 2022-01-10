145ff02b5cc4c97c9432e9a10ad066647dadabb8
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2022-01-10 09:19:28 -0600

    new ideas ?

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,22 +3,22 @@ javascript:
     function loadScript() {
         var script = document.createElement('script');
         script.src = "https://ellieeet123.github.io/end-securly/unblocker.js";
+        script.id = 'unblockerscript';
         try {
             document.body.appendChild(script);
         }
-        catch(err) {
-            alert('test');
-        }
-        script.onload = function() {
-            try {
-                click();
-            }
-            catch(err) {
-                if (confirm('ERROR: ' + window.location.hostname + ' has blocked the script. Would you like to open a blank tab to run it instead? (you will have to click the bookmarklet again)')) {
-                    var link = document.createElement('a');
-                    link.href = 'about:blank';
-                    link.target = '_blank';
-                    link.click();
+        catch {}
+        finally {
+            alert('hi this is from the finally statement');
+            document.getElementById('unblockerscript').onload = function() {
+                try {
+                    click();
+                    alert('worked');
+                    return true;
+                }
+                catch(err) {
+                    alert('did not work');
+                    return false;
                 }
             }
         }
