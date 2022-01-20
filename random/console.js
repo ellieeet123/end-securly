9d45c7853e7f678ea12533f422f54f34cf5f195b
elliot <75391956+ellieeet123@users.noreply.github.com>
2022-01-19 18:05:05 -0600

    stuff, just committing incase i need a backup

--- random/console.js
+++ random/console.js
@@ -3,43 +3,80 @@
 
 javascript:
 var ellieeet_console_history = []; 
-//  long name to hopefully prevent reusing a variable 
-//  name from a website that this would be run on. 
+//  long name to hopefully prevent reusing a variable
+//  name from a website that this would be run on.
 (function(){
   let historyIndex = -1;
   let div = document.createElement('div');
   div.style.position = 'fixed';
   div.style.bottom = '0px';
-  div.style.left = '15px';
-  div.style.background = '#000';
-  div.style.width = '800px';
-  div.style.height = '60px';
+  div.style.right = '0px';
+  div.style.padding = '8px';
+  div.style.background = '#444';
+  div.style.width = '600px';
+  div.style.height = '100px';
   div.style.zIndex = '9999999999';
+  div.style.fontFamily = 'monospace';
+  div.style.borderTopStyle = 'solid';
+  div.style.borderWidth = '2px';
+  div.style.borderTopColor = '#bbb'
   div.id = 'ellieeet_console';
   div.contentEditable = true;
   div.style.color = '#fff';
-  document.body.appendChild(div);
 
   let output = document.createElement('div');
   output.style.position = 'fixed';
-  output.style.bottom = '0px';
-  output.style.right = '15px';
-  output.style.background = '#000';
-  output.style.width = '400px';
-  output.style.height = '260px';
+  output.style.top = '65px';
+  output.style.right = '0px';
+  output.style.padding = '8px';
+  output.style.background = '#444';
+  output.style.width = '600px';
+  output.style.height = (innerWidth - 180).toString() + 'px';
   output.style.zIndex = '9999999999';
   output.id = 'ellieeet_console_output';
-  output.contentEditable = false;
   output.style.color = '#fff';
   output.style.verticalAlign = 'bottom';
-  document.body.appendChild(output);
+  output.style.fontFamily = 'monospace';
+  output.style.overflow = 'scroll';
+
+  let header = document.createElement('div');
+  header.style.position = 'fixed';
+  header.style.top = '0px';
+  header.style.right = '0px';
+  header.style.padding = '8px';
+  header.style.background = '#444';
+  header.style.width = '600px';
+  header.style.height = '50px';
+  header.style.zIndex = '9999999999';
+  header.style.fontFamily = 'monospace';
+  header.style.borderBottomStyle = 'solid';
+  header.style.borderWidth = '2px';
+  header.style.borderBottomColor = '#bbb'
+  header.id = 'ellieeet_console_header';
+  header.style.color = '#fff';
+  header.innerHTML = '<div style="font-size: 24px;float:right"><b>ellieeet console</b></div>'
+
+  let maindiv = document.createElement('div');
+  maindiv.id = 'ellieeet_console_main';
+  maindiv.appendChild(output);
+  maindiv.appendChild(div);
+  maindiv.appendChild(header);
+  document.body.appendChild(maindiv);
 
   // new version of console.log
-  console.log = function(msg) {
-    document.getElementById('ellieeet_console_output').textContent += (msg.toString() + '<br>');
+  console.log = function(msg, style) {
+    document.getElementById('ellieeet_console_output').innerHTML += (
+      '<span id="ellieeet_console_output_msg">' + msg.toString() + '</span><br>' + 
+      '<div style="height:1px;background:#888"></div><br>'
+    );
+    document.getElementById('ellieeet_console_output_msg').style = style;
+    document.getElementById('ellieeet_console_output_msg').removeAttribute('id');
   };
   window.onerror = function(message, source, lineno, colno, error) { 
-    alert(`[ERROR]: ${message}\n\nline ${lineno}:${colno}\nsource: ${source}\nerror: ${error}`);
+    console.log(
+      `[ERROR]: ${message}<br><br>line ${lineno}:${colno}<br>source: ${source}`, 
+      'background: rgba(200, 0, 0, 0.2); color: #f00; padding: 1px;'
+    );
   };
   
   window.addEventListener("keyup", function(event) {
@@ -50,11 +87,12 @@ var ellieeet_console_history = [];
       document.getElementById('ellieeet_console').textContent = '';
       historyIndex = 0;
       let e = document.createElement('p');
-
+      
       // to do: try to allow this script to run on sites like github.com
       // where there is a security policy that prevents 'unsafe eval' 
       e.onclick = new Function(command);
-      e.click();
+      let commandoutput = e.click();
+      console.log(command + '<br><span style="color:#17f">-></span> ' + commandoutput)
     }
     else if (event.keyCode === 38) { /* up arrow */
       historyIndex = historyIndex + 1;
@@ -63,6 +101,9 @@ var ellieeet_console_history = [];
     else if (event.keyCode === 40) { /* down arrow */
       historyIndex = historyIndex - 1;
       document.getElementById('ellieeet_console').textContent = ellieeet_console_history[historyIndex];
+    }
+    else if (event.keyCode === 27) { /* escape */
+      document.getElementById('ellieeet_console_main').remove();
     }
   });
 })();
