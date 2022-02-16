26441f86e35bc1e86fa9cdf64b6229242cdd19bb
elliot <75391956+ellieeet123@users.noreply.github.com>
2022-02-15 20:05:10 -0600

    Update rand_string.js

--- random/rand_string.js
+++ random/rand_string.js
@@ -26,8 +26,8 @@ s36wcp57cb7ru7p4 [OLD]
 gn6ih7aa0i5rdih7 [OLD]
 cc8o9ffyjh3xvrbu [CURRENT - MAIN]
 xhq2c04ncpk3ye7e [OLD]
-xemhx53e5gumbva3 [CURRENT - PRINGLES]
-mhzwesscixgzlgr5
+xemhx53e5gumbva3 [OLD]
+mhzwesscixgzlgr5 [CURRENT - PRINGLES]
 ymbbmgh8vxvyoxvd
 brepqkc6d699hqh8
 hrqa6iy6j6hn4fjm
