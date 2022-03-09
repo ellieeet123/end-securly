ed7d24dba1f248203ed5e8c81265fc4e2352c0ad
elliot <75391956+ellieeet123@users.noreply.github.com>
2022-03-09 13:01:12 -0600

    Improve bookmarklet

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -7,33 +7,44 @@
 \*/
 
 javascript:
-(function() {
-    var bookmarkletClicked;
-    function loadScript() {
-        document.addEventListener("securitypolicyviolation", (e) => {
-            if (confirm('[ERROR]: ' + window.location.hostname + 
-            ' has blocked inserting the necesary script for the unblocker. Would you like to open a blank tab to run it instead? (you will have to double click the bookmarklet again.) \n\n Full Error Report: '
-            + e.originalPolicy)) {
-                var a = document.createElement('a');
-                a.href = 'about:blank';
-                a.target = '_blank';
-                a.click()
-                bookmarkletClicked = false;
-            }
-        });
+(() => {
+    document.addEventListener("securitypolicyviolation", err => {
+        // error message if the site the user is trying to run the script
+        // on a site that has a security policy disabling foreign scripts.
+        if (
+            confirm(
+                '[ERROR]: ' + window.location.hostname + 
+                ' has blocked inserting the necesary script for the unblocker.' + 
+                ' To run it, press OK (this will open a new tab)' + 
+                ' and then press the bookmarklet again. \n\nFull Security Policy: ' + 
+                err.originalPolicy
+            )
+        ) {
+            // opens a new tab to about:blank, so that a new proxy can be
+            // opened there.
+            var popupLink = document.createElement('a');
+            popupLink.href = 'about:blank';
+            popupLink.target = '_blank';
+            popupLink.click();
+            bookmarkletClicked = false;
+        }
+    });
+    // check if the unblocker script already exists
+    if (!document.querySelector('#_chicken_wing_')) {
+        // the first time the bookmarklet is clicked, the script has
+        // to be loaded first.
         var script = document.createElement('script');
         script.src = "https://ellieeet123.github.io/end-securly/src/unblocker.js";
-        script.id = 'unblockerscript';
+        script.id = '_chicken_wing_'; // random name to hopefully avoid duplicate IDs in the DOM
         document.body.appendChild(script);
-        script.onload = function() {
+        // click() is a function from the inserted script, it starts the main menu UI.
+        // this is run once the script is loaded.
+        script.onload = () => {
             click();
-        }
-    }   
-    if (!bookmarkletClicked) {
-        bookmarkletClicked = true;
-        loadScript();
+        };
     }
     else {
+        // immediately run the click function if script has already been loaded. 
         click();
     }
 })();
